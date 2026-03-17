7d247d5e498dd06e05f7b09c5d2c24bea54c64b2
acqarai-tech <acqarai@gmail.com>
2026-03-17 16:53:40 +0500

    Update App.jsx

--- acqar-rems/frontend/src/App.jsx
+++ acqar-rems/frontend/src/App.jsx
@@ -10,7 +10,11 @@ export default function App() {
       <EventsProvider>
         <Routes>
             <Route path="/" element={<Landing />} />
-          <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/dashboard" element={
+  <ProtectedRoute>
+    <Dashboard />
+  </ProtectedRoute>
+} />
         </Routes>
       </EventsProvider>
     </SocketProvider>
