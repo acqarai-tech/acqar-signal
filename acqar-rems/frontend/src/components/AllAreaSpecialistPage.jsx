6434b3287542c47b914bed4186937b7c142fe586
acqarai-tech <acqarai@gmail.com>
2026-05-15 11:09:50 +0500

    Update AllAreaSpecialistPage.jsx

--- acqar-rems/frontend/src/components/AllAreaSpecialistPage.jsx
+++ acqar-rems/frontend/src/components/AllAreaSpecialistPage.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect, useRef } from 'react'
 import { useEvents } from '../context/EventsContext'
-import TickerBar from './TickerBar'
+import TickerBar from './TickkerBar'
 
 
 
