8dad6f2caf27553c5d615b765d29e31e11229901
acqarai-tech <acqarai@gmail.com>
2026-05-25 12:57:58 +0500

    Update analytics.js

--- acqar-rems/frontend/src/analytics.js
+++ acqar-rems/frontend/src/analytics.js
@@ -15,18 +15,19 @@
 
 
 
-import ReactGA from "react-ga4";
-import posthog from "posthog-js";
+import posthog from 'posthog-js'
 
-export const initGA = () => {
-  ReactGA.initialize("G-3ZRKF69290");
-};
+posthog.init(import.meta.env.VITE_POSTHOG_KEY, {
+  api_host: 'https://app.posthog.com',
+  capture_pageview: false,
+})
 
-export const trackPage = (path) => {
-  ReactGA.send({ hitType: "pageview", page: path, title: document.title });
-};
+export function trackPage(path) {
+  posthog.capture('$pageview', { path })
+}
 
-export const trackEvent = (eventName, params = {}) => {
-  ReactGA.event(eventName, params);
+export function trackEvent(eventName, params = {}) {
+  posthog.capture(eventName, params)
+}
   posthog.capture(eventName, params);
 };
